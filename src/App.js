b59b9b770818c8871d5ffcdded89313655915405
Khambazarov <khambazarovrenat@gmail.com>
2021-10-11 11:13:42 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>pier</h2>
-      <h2>giacomo</h2>
+      <h2>Pier</h2>
+      <h2>Giacomo</h2>
       <h2>Renat</h2>
     </div>
   );
