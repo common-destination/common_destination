07560547fc157c13436f233cb121c4a3c0923822
Khambazarov <khambazarovrenat@gmail.com>
2021-10-11 10:57:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 
 function App() {
   return <div className='App'>pier</div>;
+  return <div className='App'>
+    giacomo
+    <h2>Renat</h2>
+    </div>;
 }
 
 export default App;
