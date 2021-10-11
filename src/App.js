bbc481d128d3da929136bcc4b50c44d671a8e3c2
Khambazarov <khambazarovrenat@gmail.com>
2021-10-11 10:59:38 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return <div className='App'>pier</div>;
-  return <div className='App'>
-    giacomo
-    <h2>Renat</h2>
-    </div>;
+  return (
+    <div className="App">
+      <h2>pier</h2>
+      <h2>giacomo</h2>
+      <h2>Renat</h2>
+    </div>
+  );
 }
 
 export default App;
