479cc0fa7d298bb31e35f2f7676f4c2943232a81
Khambazarov <khambazarovrenat@gmail.com>
2022-01-25 12:30:35 +0100

    create Caledar.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Caledar from "./flightsSearch/Caledar";
 import SelectDepartureAirport from "./flightsSearch/SelectDepartureAirport";
 // import { useTheme } from "../ThemeContext";
 
 function Home(props) {
   const [arrPassengers, setArrPassengers] = useState([""]);
+  const [departureAirport, setDepartureAirport] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const requestOptions = {
+        method: "GET",
+        credentials: "include",
+      };
+      // console.log("requestOptions", requestOptions);
+      const response = await fetch(
+        `http://localhost:8033/flights/airports`,
+        requestOptions
+      );
+      // console.log("response", response);
+      if (response.ok) {
+        const _departureAirport = await response.json();
+        setDepartureAirport(_departureAirport);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log("departureAirport", departureAirport);
 
   return (
     <div className={props.className}>
-      {arrPassengers.map((passenger) => (
-        <div>
+      {arrPassengers.map((passenger, index) => (
+        <div key={index}>
           <SelectDepartureAirport />
+          <Caledar />
         </div>
       ))}
       <button
@@ -18,6 +42,9 @@ function Home(props) {
       >
         Add new Passenger
       </button>
+      <form>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
--- src/components/flightsSearch/SelectDepartureAirport.jsx
+++ src/components/flightsSearch/SelectDepartureAirport.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { useTheme } from "../../ThemeContext.js";
+// import { useTheme } from "../../ThemeContext.js";
 
 function SelectDepartureAirport() {
-  const { mediaQueries } = useTheme();
+  // const { mediaQueries } = useTheme();
 
   return (
     <div>
@@ -16,35 +16,6 @@ function SelectDepartureAirport() {
         <label>
           <h5>From</h5>
         </label>
-      </div>
-      <form>
-        <div>
-          <input
-            type="search"
-            name="from"
-            id="from"
-            placeholder="Select your Airport"
-            autoComplete="true"
-            autoFocus={mediaQueries.smallView ? true : false}
-          />
-        </div>
-        <div>
-          <div>
-            <label>
-              <h5>Depart</h5>
-              <input type="date" name="date" />
-            </label>
-          </div>
-          <div>
-            <label>
-              <h5>Return</h5>
-            </label>
-          </div>
-        </div>
-        <input type="date" name="date" />
-      </form>
-      <div>
-        <button>Submit</button>
       </div>
     </div>
   );
