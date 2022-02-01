b275c1802a5d3a6603fe6b3fb5aed390f29870ff
Khambazarov <khambazarovrenat@gmail.com>
2022-02-01 10:25:54 +0100

    add min & max number to InputNumber

--- src/components/flightsSearch/InputNumber.jsx
+++ src/components/flightsSearch/InputNumber.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-function inputNumber(props) {
+function InputNumber(props) {
   return (
     <>
       <label>
         <h5>min stay time</h5>
-        <input className="minimumJourney" type="number" defaultValue={1} />
+        <input
+          className="minimumJourney"
+          type="number"
+          min="1"
+          max="30"
+          defaultValue={1}
+        />
       </label>
     </>
   );
 }
 
-export default inputNumber;
+export default InputNumber;
