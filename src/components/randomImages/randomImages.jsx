c4da01634bee026e39f101ee6b2b1211980362b7
pierluigi <pierluigibaiano@gmail.com>
2022-02-12 16:32:07 +0100

    add switch case. Works

--- src/components/randomImages/randomImages.jsx
+++ src/components/randomImages/randomImages.jsx
@@ -8,17 +8,34 @@ import img4 from "../../assets/images/airport4-min.jpg";
 
 function RandomImages({ className }) {
   const refAppBg = useRef(null);
+
   const pictureArray = [img0, img1, img2, img3, img4];
   const randomIndex = Math.floor(Math.random() * pictureArray.length);
   const selectedPicture = pictureArray[randomIndex];
   console.log(randomIndex);
+
   useEffect(() => {
     // console.log(selectedPicture);
     refAppBg.current.style.backgroundImage = `url(${selectedPicture})`;
 
-    refAppBg.current.style.backgroundPosition =
-      (selectedPicture === img0 && "30% 50%") ||
-      (selectedPicture === img1 && "center center");
+    let backgroundPosition = () => {
+      switch (selectedPicture) {
+        case img0:
+          return "0% 30%";
+        case img1:
+          return "10% 30%";
+        case img2:
+          return "center center";
+        case img3:
+          return "30% 70%";
+        case img4:
+          return "40% 10%";
+        default:
+          return "29% 29%";
+      }
+    };
+    refAppBg.current.style.backgroundPosition = backgroundPosition();
+    // console.log(backgroundPosition());
   }, [selectedPicture]);
 
   return <div className={className} ref={refAppBg}></div>;
