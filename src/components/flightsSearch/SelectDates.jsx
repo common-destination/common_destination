a8af3f131a1160f53d50895063408f8f7050753a
giacomotolari <63161008+giacomotolari@users.noreply.github.com>
2022-02-14 16:10:38 +0100

    Gia fix home validation (#50)
    * change popUP component
    
    * create function that disable departure before than return date
    
    * change small Components
    
    * finish with validations in home
    
    * finish
    
    * cancell comment
    
    * fix conflicts problem by scss
    
    Co-authored-by: giacomotolari <tolarigiacomo@gmail.com>

--- src/components/flightsSearch/SelectDates.jsx
+++ src/components/flightsSearch/SelectDates.jsx
@@ -119,7 +119,7 @@ dateAreValid ? setOtboundLaterThanReturn(true) : setOtboundLaterThanReturn(false
         timeFormat="HH:mm"
         timeIntervals={60}
         dateFormat="dd-MMM-yyyy HH:mm"
-        // disabled={minOutboundDate === "" ? true : false}
+
       />
     </div>
   );
