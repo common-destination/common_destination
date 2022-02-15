7c86cf1cd8a8e3a81b24ad2627bcedf015f6680f
Renat Khambazarov <77329064+Khambazarov@users.noreply.github.com>
2022-02-15 16:13:03 +0100

    add img of cities and style home... (#54)

--- src/functions/images.js
+++ src/functions/images.js
@@ -2,14 +2,70 @@ import airplaneDown from "../assets/gifs/airplaneDown.gif";
 import airplaneDownStopped from "../assets/gifs/airplaneDownStopped.png";
 import airplaneUp from "../assets/gifs/airplaneUp.gif";
 import airplaneUpStopped from "../assets/gifs/airplaneUpStopped.png";
-
+import Amsterdam from "../assets/cities/Amsterdam.jpg";
+import Athens from "../assets/cities/Athens.jpg";
+import Barcelona from "../assets/cities/Barcelona.jpg";
+import Berlin from "../assets/cities/Berlin.jpg";
+import Brussels from "../assets/cities/Brussels.jpg";
+import Budapest from "../assets/cities/Budapest.jpg";
+import Copenhagen from "../assets/cities/Copenhagen.jpg";
+import Dublin from "../assets/cities/Dublin.jpg";
+import Dusseldorf from "../assets/cities/Dusseldorf.jpg";
+import Frankfurt from "../assets/cities/Frankfurt.jpg";
+import Hamburg from "../assets/cities/Hamburg.jpg";
+import Helsinki from "../assets/cities/Helsinki.jpg";
+import Istanbul from "../assets/cities/Istanbul.jpg";
+import Lisbon from "../assets/cities/Lisbon.jpg";
+import London from "../assets/cities/London.jpg";
+import Mallorca from "../assets/cities/Mallorca.jpg";
+import Milan from "../assets/cities/Milan.jpg";
+import Moscow from "../assets/cities/Moscow.jpg";
+import Munich from "../assets/cities/Munich.jpg";
+import Oslo from "../assets/cities/Oslo.jpg";
+import Paris from "../assets/cities/Paris.jpg";
+import Prague from "../assets/cities/Prague.jpg";
+import Rome from "../assets/cities/Rome.jpg";
+import SaintPetersburg from "../assets/cities/Saint-Petersburg.jpg";
+import Stockholm from "../assets/cities/Stockholm.jpg";
+import Stuttgart from "../assets/cities/Stuttgart.jpg";
+import Vienna from "../assets/cities/Vienna.jpg";
+import Warsaw from "../assets/cities/Warsaw.jpg";
+import Zurich from "../assets/cities/Zurich.jpg";
 
 const images = {
   airplaneDown,
   airplaneDownStopped,
   airplaneUpStopped,
   airplaneUp,
-
+  Amsterdam,
+  Athens,
+  Barcelona,
+  Berlin,
+  Brussels,
+  Budapest,
+  Copenhagen,
+  Dublin,
+  Dusseldorf,
+  Frankfurt,
+  Hamburg,
+  Helsinki,
+  Istanbul,
+  Lisbon,
+  London,
+  Milan,
+  Moscow,
+  Munich,
+  Oslo,
+  Mallorca,
+  Paris,
+  Prague,
+  Rome,
+  SaintPetersburg,
+  Stockholm,
+  Stuttgart,
+  Vienna,
+  Warsaw,
+  Zurich,
 };
 
 export default images;
