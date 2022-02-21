b387b874b89714207aa5249bcef52e4b999e94d5
Renat Khambazarov <77329064+Khambazarov@users.noreply.github.com>
2022-02-21 12:54:00 +0100

    add style to hme.scss (#66)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import FlightSearch from "./flightsSearch/FlightSearch";
 function Home({ className }) {
   return (
     <div className={className}>
-      <h2> </h2>
+      <h2 className="afterEffect"> </h2>
       <FlightSearch />
     </div>
   );
