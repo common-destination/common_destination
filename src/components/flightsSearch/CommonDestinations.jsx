9e2281f39e044903452071bf84811d7cd9b45afa
Renat Khambazarov <77329064+Khambazarov@users.noreply.github.com>
2022-02-22 01:13:46 +0100

    Re feat function to get days from hours (#69)
    * fix bug
    
    * Navbar has opacity 0.7
    
    * fix bugs
    
    * create fn to get days instead hours

--- src/components/flightsSearch/CommonDestinations.jsx
+++ src/components/flightsSearch/CommonDestinations.jsx
@@ -24,6 +24,21 @@ function CommonDestinations() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const convertHours = (timeTogether) => {
+    const day = Math.floor(timeTogether / 24);
+    const hour = Math.floor(timeTogether % 24);
+    if (day === 0 && hour === 1) return `${hour} hour`;
+    else if (day === 0 && hour > 1) return `${hour} hours`;
+    else if (day === 1 && hour === 0) return `${day} day`;
+    else if (day === 1 && hour === 1) return `${day} day ${hour} hour`;
+    else if (day === 1 && hour > 1) return `${day} day ${hour} hours`;
+    else if (day > 1 && hour === 0) return `${day} days`;
+    else if (day > 1 && hour === 1) return `${day} days ${hour} hour`;
+    else if (day > 1 && hour > 1) return `${day} days ${hour} hours`;
+    else return `${day} days ${hour} hours, please check again`;
+  };
+  convertHours();
+
   return (
     <div className="commonDestinations">
       {commonDestinations.length > 0 && (
@@ -44,7 +59,7 @@ function CommonDestinations() {
                       }}
                     >
                       <icons.FaClock />
-                      {commonDestinationToAirport.timeTogether}
+                      {convertHours(commonDestinationToAirport.timeTogether)}
                       <icons.FaEuroSign />
                       {commonDestinationToAirport.groupPrice}
                     </li>
